4e9fc3720432ee3e46bc2aee3df27ff03d8a46d7
John Placek <john.placek@agnos.ai>
2022-12-28 13:18:50 +0000

    failure test scenario

--- mustrd.py
+++ mustrd.py
@@ -24,11 +24,23 @@ class Then:
 
 
 @dataclass
-class ScenarioPass:
-    scenarioUri: URIRef
+class GraphComparison:
+    in_expected_not_in_actual: Graph
+    in_actual_not_in_expected: Graph
+    in_both: Graph
 
 
-def run_scenario(scenario_graph: Graph, g: Given, w: When) -> ScenarioPass:
+@dataclass
+class ScenarioResult:
+    scenario_uri: URIRef
+
+
+@dataclass
+class ScenarioFailure(ScenarioResult):
+    graph_comparison: GraphComparison
+
+
+def run_scenario(scenario_graph: Graph, g: Given, w: When) -> ScenarioResult:
     scenario_uri = list(scenario_graph.subjects(RDF.type, MUST.TestScenario))[0]
     scenario_query = f"""
     CONSTRUCT {{?then ?p1 ?o1 . ?o1 ?p2 ?o2 . }} 
@@ -53,17 +65,23 @@ def run_scenario(scenario_graph: Graph, g: Given, w: When) -> ScenarioPass:
             g.add((column_node, MUST.variable, Literal(key)))
             g.add((column_node, MUST.binding, value))
 
-    message = error_message(scenario, g)
+    graph_compare = graph_comparison(scenario, g)
     equal = isomorphic(g, scenario)
     if equal:
-        return ScenarioPass(scenario_uri)
+        return ScenarioResult(scenario_uri)
+    else:
+        return ScenarioFailure(scenario_uri, graph_compare)
 
 
-def error_message(expected_graph, actual_graph) -> str:
+def graph_comparison(expected_graph, actual_graph) -> GraphComparison:
     diff = graph_diff(expected_graph, actual_graph)
     in_both = diff[0]
     in_expected = diff[1]
     in_actual = diff[2]
-    in_expected_not_in_actual = (in_expected - in_actual).serialize(format='ttl')
-    in_actual_not_in_expected = (in_actual - in_expected).serialize(format='ttl')
-    return f"in_expected_not_in_actual\n{in_expected_not_in_actual}\nin_actual_not_in_expected\n{in_actual_not_in_expected}\nin_both\n{in_both}"
+    in_expected_not_in_actual = (in_expected - in_actual)
+    in_actual_not_in_expected = (in_actual - in_expected)
+    # in_expected_not_in_actual = (in_expected - in_actual).serialize(format='ttl')
+    # in_actual_not_in_expected = (in_actual - in_expected).serialize(format='ttl')
+    # in_both = diff[0].serialize(format='ttl')
+    # message = f"in_expected_not_in_actual\n{in_expected_not_in_actual}\nin_actual_not_in_expected\n{in_actual_not_in_expected}\nin_both\n{in_both}"
+    return GraphComparison(in_expected_not_in_actual, in_actual_not_in_expected, in_both)
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from mustrd import Given, When, run_scenario, ScenarioPass
+from mustrd import Given, When, run_scenario, ScenarioResult, ScenarioFailure
 from rdflib import Graph, URIRef
 
 
@@ -44,6 +44,61 @@ class TestMustrd:
 
         t = run_scenario(scenario_graph, g, w)
 
-        expected_result = ScenarioPass(URIRef("https://semanticpartners.com/data/test/my-first-scenario"))
+        expected_result = ScenarioResult(URIRef("https://semanticpartners.com/data/test/my-first-scenario"))
         assert t == expected_result
+
+    def test_select_scenario_fails_with_message(self):
+        triples = """
+        @prefix test-data: <https://semanticpartners.com/data/test/> .
+        test-data:sub test-data:pred test-data:obj .
+        """
+        state = Graph()
+        state.parse(data=triples, format="ttl")
+        select_query = """
+        select ?s ?p ?o { ?s ?p ?o }
+        """
+        scenario_graph = Graph()
+        scenario = """
+        @prefix sh: <http://www.w3.org/ns/shacl#> .
+        @prefix must: <https://semanticpartners.com/mustrd/> .
+        @prefix test-data: <https://semanticpartners.com/data/test/> .
+        
+        test-data:my-failing-scenario 
+            a must:TestScenario ;
+            must:then [
+                sh:order 1 ;
+                must:results [
+                   must:variable "s" ;
+                   must:binding test-data:wrong-subject ; 
+                    ] ,
+                    [
+                   must:variable "p" ;
+                   must:binding test-data:pred ; 
+                    ] ,
+                    [
+                   must:variable "o" ;
+                   must:binding test-data:obj  ; 
+                    ];
+                ] .
+        """
+        scenario_graph.parse(data=scenario, format='ttl')
+
+        g = Given(state)
+        w = When(select_query)
+
+        scenario_result = run_scenario(scenario_graph, g, w)
+
+        if type(scenario_result) == ScenarioFailure:
+            graph_comparison = scenario_result.graph_comparison
+            assert scenario_result.scenario_uri == URIRef("https://semanticpartners.com/data/test/my-failing-scenario")
+            assert graph_comparison.in_expected_not_in_actual.serialize(format='ttl') == """@prefix ns1: <https://semanticpartners.com/mustrd/> .
+
+[] ns1:binding <https://semanticpartners.com/data/test/wrong-subject> .
+
+"""
+            assert graph_comparison.in_actual_not_in_expected.serialize(format='ttl') == """@prefix ns1: <https://semanticpartners.com/mustrd/> .
+
+[] ns1:binding <https://semanticpartners.com/data/test/sub> .
+
+"""
 
