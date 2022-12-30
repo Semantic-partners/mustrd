01118267f1cd84452a0dbc1479a2c3b90f05da94
John Placek <john.placek@agnos.ai>
2022-12-30 16:22:05 +0000

    improved output to main. Added verbose.

--- src/run.py
+++ src/run.py
@@ -1,6 +1,6 @@
 import sys
 import getopt
-from mustrd import run_specs, SpecPassed
+from mustrd import run_specs, SpecPassed, SelectSpecFailure, ConstructSpecFailure
 from pathlib import Path
 from colorama import Fore, Style
 
@@ -8,24 +8,46 @@ from colorama import Fore, Style
 def main(argv):
 
     path_under_test = None
-    opts, args = getopt.getopt(argv, "hp:", ["put="])
+    verbose = False
+    opts, args = getopt.getopt(argv, "hvp:", ["put="])
     for opt, arg in opts:
         if opt == '-h':
             print('run.py -p <path_under_test>')
             sys.exit()
         elif opt in ("-p", "--put"):
             path_under_test = arg
+        if opt in ("-v", "--verbose"):
+            print("Verbose set")
+            verbose = True
     if not path_under_test:
         sys.exit("path_under_test not set")
-    print('Path under test is ', path_under_test)
+    print('Path under test is', path_under_test)
 
     results = run_specs(Path(path_under_test))
+    pass_count = 0
+    fail_count = 0
     for res in results:
         if type(res) == SpecPassed:
             colour = Fore.GREEN
+            pass_count += 1
         else:
             colour = Fore.RED
+            fail_count += 1
         print(f"{res.spec_uri} {colour}{type(res).__name__}{Style.RESET_ALL}")
+
+    overview_colour = Fore.GREEN
+    if fail_count:
+        overview_colour = Fore.RED
+    print(f"{overview_colour}===== {fail_count} failures, {Fore.GREEN}{pass_count} passed{overview_colour} =====")
+
+    if verbose and fail_count:
+        for res in results:
+            if type(res) == SelectSpecFailure:
+                print(f"Failed {res.spec_uri}")
+                print(res.table_comparison.to_markdown())
+            if type(res) == ConstructSpecFailure:
+                print(f"Failed {res.spec_uri}")
+
 
 
 if __name__ == "__main__":
