76d9566336414e7555d7d72b489a874054572ebf
apicouSP <aymeric.picou@semanticpartners.com>
2023-01-11 19:19:57 +0100

    Init MustrdAnzo

---
+++ src/mustrdAnzo.py
@@ -0,0 +1,47 @@
+import logging
+import requests
+from pyanzo.graphmart_manager import GraphmartManager
+from pyanzo import AnzoClient
+
+class MustrdAnzo:
+    def __init__(self,anzoUrl, anzoPort, username=None, password=None):
+        self.anzoUrl = anzoUrl
+        self.anzoPort = anzoPort
+        self.username = username
+        self.password = password
+        self.anzo_client = AnzoClient(self.anzoUrl, self.anzoPort, self.username,self.password)
+
+    def getSpecItemGraphmart(self, graphMart,layers=None):
+            return self.anzo_client.query_graphmart(graphmart=graphMart,data_layers=layers,query_string="CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o}", skip_cache=True).as_quad_store()
+
+    def getQueryFromQueryBuilder(self,folderName, queryName):
+        query = f"""SELECT ?query WHERE {{
+            graph ?queryFolder {{
+                ?bookmark a <http://www.cambridgesemantics.com/ontologies/QueryPlayground#QueryBookmark>;
+                            <http://openanzo.org/ontologies/2008/07/System#query> ?query;
+                            <http://purl.org/dc/elements/1.1/title> "{queryName}"
+                }}
+                ?queryFolder a <http://www.cambridgesemantics.com/ontologies/QueryPlayground#QueryFolder>;
+                            <http://purl.org/dc/elements/1.1/title> "{folderName}"
+        }}"""
+        return self.anzo_client.query_journal(query_string = query).as_table_results().as_record_dictionaries()[0].get("query")
+
+    def getQueryFromStep(self,queryStepUri): 
+        query = f"""SELECT ?query WHERE {{
+            BIND(<{queryStepUri}> as ?stepUri)
+            graph ?stepUri {{
+                ?stepUri a <http://cambridgesemantics.com/ontologies/Graphmarts#Step>; <http://cambridgesemantics.com/ontologies/Graphmarts#transformQuery> ?query
+            }}
+        }}
+        """
+        return self.anzo_client.query_journal(query_string = query).as_table_results().as_record_dictionaries()[0].get("query")
+
+    def uploadGiven(self, givenContent, gqeUri, layerUri):
+        insertQuery = f"INSERT DATA {{graph <{layerUri}>{{{givenContent}}}}}"
+        data = {'datasourceURI' : gqeUri, 'update': insertQuery}
+        requests.post(url=f"https://{self.anzoUrl}:{self.anzoPort}/sparql", auth = (self.username, self.password), data = data)
+
+    def executeWhenAgainstGiven(self, gqeUri, layerUri, whenQuery):
+        data = {'datasourceURI' : gqeUri, 'query': whenQuery, 'default-graph-uri': layerUri}
+        return requests.post(url=f"https://{self.anzoUrl}:{self.anzoPort}/sparql?format=test/csv", auth = (self.username, self.password), data = data).content
+
