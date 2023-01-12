f54a813dd1f542358185fef2284678ad24113149
apicouSP <aymeric.picou@semanticpartners.com>
2023-01-12 17:23:42 +0100

    manage different item sources and different triple store

---
+++ src/mustrdRdfLib.py
@@ -0,0 +1,11 @@
+import logging
+
+
+class MustrdRdfLib:
+    def __init__(self):
+        pass
+
+    def uploadGiven(self, given):
+        pass
+    def executeWhenAgainstGiven(self,when):
+        pass
