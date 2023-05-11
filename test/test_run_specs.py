e18ed62c73425d219863a5d4f06273910a831ff4
aplarge <andy.large@agnos.ai>
2023-05-11 12:05:36 +0100

    added new tests to to test_run_specs for implementing updates queries for graphDB

--- test/test_run_specs.py
+++ test/test_run_specs.py
@@ -22,6 +22,8 @@ class TestRunSpecs:
             SpecPassed(URIRef(TEST_DATA.a_complete_construct_scenario_with_variables), MUST.rdfLib),
             SpecPassed(URIRef(TEST_DATA.a_complete_delete_data_scenario), MUST.rdfLib),
             SpecPassed(URIRef(TEST_DATA.a_complete_delete_insert_scenario), MUST.rdfLib),
+            SpecPassed(URIRef(TEST_DATA.a_complete_delete_insert_with_optional_scenario), MUST.rdfLib),
+            SpecPassed(URIRef(TEST_DATA.a_complete_delete_insert_with_subselect_scenario), MUST.rdfLib),
             SpecPassed(URIRef(TEST_DATA.a_complete_delete_scenario), MUST.rdfLib),
             SpecPassed(URIRef(TEST_DATA.a_complete_insert_data_scenario), MUST.rdfLib),
             SpecPassed(URIRef(TEST_DATA.a_complete_insert_scenario), MUST.rdfLib),
