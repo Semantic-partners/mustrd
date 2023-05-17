89106acd90da06d15145901f740f216df94cc87d
apicouSP <aymeric.picou@semanticpartners.com>
2023-05-17 15:58:19 +0200

    Use defautl graph uri in GDB instead of parsing query to inject graph

--- src/mustrdGraphDb.py
+++ src/mustrdGraphDb.py
@@ -21,12 +21,6 @@ def manage_graphdb_response(response) -> str:
     else:
         raise Exception(f"GraphDb error, status code: {response.status_code}, content: {content_string}")
 
-
-# wrap the entire select in another select to add the graph, allows for a more complicated where
-def insert_graph(when, input_graph):
-    new_when = f"SELECT * WHERE {{ GRAPH <{input_graph}> {{ {when} }} }}"
-    return new_when
-
 # https://github.com/Semantic-partners/mustrd/issues/22
 def upload_given(triple_store: dict, given: Graph):
     try:
@@ -40,13 +34,6 @@ def upload_given(triple_store: dict, given: Graph):
     except ConnectionError:
         raise
 
-def parse_bindings(bindings: dict):
-    bindings_string = ""
-    for key, value in bindings.items():
-        encoded_value = urllib.parse.quote_plus(f'{value.n3()}')
-        bindings_string += f'&${key}={encoded_value}'
-    return bindings_string
-
 
 # https://github.com/Semantic-partners/mustrd/issues/122
 def execute_select(triple_store: dict, given: Graph, when: str, bindings: dict = None) -> str:
@@ -54,68 +41,26 @@ def execute_select(triple_store: dict, given: Graph, when: str, bindings: dict =
         drop_default_graph(triple_store)
     else:
         clear_graph(triple_store)
-        when = insert_graph(when, triple_store["input_graph"])
     upload_given(triple_store, given)
     return post_query(triple_store, when, "application/sparql-results+json", bindings)
-
-def _contains_clause(clause_type, query) -> bool:
-    if re.search(f"(?i){clause_type}[ ]*{{.+?}}",query):
-        return True
-    else:
-        return False
 
 def execute_construct(triple_store: dict, given: Graph, when: str, bindings: dict = None) -> Graph:
     if triple_store["input_graph"] is None:
         drop_default_graph(triple_store)
     else:
         clear_graph(triple_store)
-        split_when = when.lower().split("where {", 1)
-        when = split_when[0] + "where { GRAPH <" + triple_store["input_graph"] + "> {" + split_when[1] + "}"
     upload_given(triple_store, given)
-    return Graph().parse(post_query(triple_store, when, "text/turtle"))
-
-def insert_graph_into_clause(clause_type, query, input_graph) -> str:
-    if clause_type == 'where':
-        start, end = re.search(f"(?i)({clause_type}[ ]*{{)(.+}})", query).groups()
-    else:
-        start, end = re.search(f"(?i)({clause_type}[ ]*{{)(.+?}})", query).groups()
-    return start + 'GRAPH <' + input_graph + '> {' + end + '} '
-
-def insert_graph_into_query(when, input_graph):
-    delete_clause = insert_clause = where_clause = ''
-    if _contains_clause('delete', when):
-        delete_clause = insert_graph_into_clause('delete', when, input_graph)
-    if _contains_clause('delete data', when):
-        delete_clause = insert_graph_into_clause('delete data', when, input_graph)
-    if _contains_clause('insert', when):
-        insert_clause = insert_graph_into_clause('insert', when, input_graph)
-    if _contains_clause('insert data', when):
-        insert_clause = insert_graph_into_clause('insert data', when, input_graph)
-    if _contains_clause('where', when):
-        where_clause = insert_graph_into_clause('where', when, input_graph)
-    return delete_clause + insert_clause + where_clause
-
+    return Graph().parse(post_query(triple_store, when, "text/turtle", bindings))
 
 def execute_update(triple_store: dict, given: Graph, when: str, bindings: dict = None) -> Graph:
-
     if triple_store["input_graph"] is None:
         drop_default_graph(triple_store)
     else:
         clear_graph(triple_store)
-        when = insert_graph_into_query(when, triple_store["input_graph"] )
     upload_given(triple_store, given)
-    bindings_string = ""
-    if bindings:
-        bindings_string = parse_bindings(bindings)
-    post_update_query(triple_store, f"{when}{bindings_string}")
+    post_update_query(triple_store, when, bindings)
 
-    #now fetch the resulting rdf
-    if triple_store["input_graph"] is None:
-        query = "CONSTRUCT {?s ?p ?o} where { ?s ?p ?o }"
-    else:
-        query=f"CONSTRUCT {{?s ?p ?o}} where {{GRAPH <{triple_store['input_graph']}> {{ ?s ?p ?o }}}}"
-
-    return  Graph().parse(data=post_query(triple_store, query, 'text/turtle'))
+    return  Graph().parse(data=post_query(triple_store, "CONSTRUCT {?s ?p ?o} where { ?s ?p ?o }", 'text/turtle'))
 
 
 def clear_graph(triple_store: dict):
@@ -126,21 +71,29 @@ def drop_default_graph(triple_store: dict):
     post_update_query(triple_store, "DROP DEFAULT")
 
 
-def post_update_query(triple_store: dict, query: str, bindings: dict = None):
+def post_update_query(triple_store: dict, query: str, params: dict = None):
+    add_graph_to_params(params, triple_store["input_graph"])
     try:
         return manage_graphdb_response(requests.post(url=f"{triple_store['url']}:{triple_store['port']}/repositories/{triple_store['repository']}/statements",
-                                                    data = query, params=bindings, auth=(triple_store['username'], triple_store['password']), headers={'Content-Type': 'application/sparql-update'}))
+                                                    data = query, params=params, auth=(triple_store['username'], triple_store['password']), headers={'Content-Type': 'application/sparql-update'}))
     except ConnectionError:
         raise
 
-def post_query(triple_store: dict, query: str, accept: str, bindings: dict = None):
+def post_query(triple_store: dict, query: str, accept: str, params: dict = None):
     headers = {
         'Content-Type': 'application/sparql-query', 
         'Accept': accept
     }
+    add_graph_to_params(params, triple_store["input_graph"])
     try:
         return manage_graphdb_response(requests.post(url=f"{triple_store['url']}:{triple_store['port']}/repositories/{triple_store['repository']}",
-                                                    data = query, params=bindings, auth=(triple_store['username'], triple_store['password']), headers=headers))
+                                                    data = query, params=params, auth=(triple_store['username'], triple_store['password']), headers=headers))
     except ConnectionError:
         raise
+
+def add_graph_to_params(params, graph):
+    if params:
+        params['default-graph-uri'] = graph
+    else:
+        params = {'default-graph-uri': graph}
 
