c38b64d6332db565c1b95d825767428132a4ab0f
apicouSP <aymeric.picou@semanticpartners.com>
2023-05-17 16:38:08 +0200

    Clean graph using graph store delete instead of  clear/drop queries

--- src/mustrdGraphDb.py
+++ src/mustrdGraphDb.py
@@ -28,6 +28,8 @@ def upload_given(triple_store: dict, given: Graph):
         if triple_store['input_graph']: 
             graph = urllib.parse.urlencode({'graph': triple_store['input_graph']})
         url = f"{triple_store['url']}:{triple_store['port']}/repositories/{triple_store['repository']}/rdf-graphs/service?{graph}"
+        # Clean graph or default before uploading data
+        requests.delete(url=url,auth=(triple_store['username'], triple_store['password']))
         manage_graphdb_response(requests.put(url=url,
                                                         auth=(triple_store['username'], triple_store['password']),data = given.serialize(format="ttl"),
                                                         headers = {'Content-Type': 'text/turtle'}))
@@ -37,42 +39,20 @@ def upload_given(triple_store: dict, given: Graph):
 
 # https://github.com/Semantic-partners/mustrd/issues/122
 def execute_select(triple_store: dict, given: Graph, when: str, bindings: dict = None) -> str:
-    if triple_store["input_graph"] is None:
-        drop_default_graph(triple_store)
-    else:
-        clear_graph(triple_store)
     upload_given(triple_store, given)
     return post_query(triple_store, when, "application/sparql-results+json", bindings)
 
 def execute_construct(triple_store: dict, given: Graph, when: str, bindings: dict = None) -> Graph:
-    if triple_store["input_graph"] is None:
-        drop_default_graph(triple_store)
-    else:
-        clear_graph(triple_store)
     upload_given(triple_store, given)
     return Graph().parse(post_query(triple_store, when, "text/turtle", bindings))
 
 def execute_update(triple_store: dict, given: Graph, when: str, bindings: dict = None) -> Graph:
-    if triple_store["input_graph"] is None:
-        drop_default_graph(triple_store)
-    else:
-        clear_graph(triple_store)
     upload_given(triple_store, given)
     post_update_query(triple_store, when, bindings)
-
     return  Graph().parse(data=post_query(triple_store, "CONSTRUCT {?s ?p ?o} where { ?s ?p ?o }", 'text/turtle'))
 
-
-def clear_graph(triple_store: dict):
-    post_update_query(triple_store, f"CLEAR GRAPH <{triple_store['input_graph']}>")
-
-
-def drop_default_graph(triple_store: dict):
-    post_update_query(triple_store, "DROP DEFAULT")
-
-
 def post_update_query(triple_store: dict, query: str, params: dict = None):
-    add_graph_to_params(params, triple_store["input_graph"])
+    params = add_graph_to_params(params, triple_store["input_graph"])
     try:
         return manage_graphdb_response(requests.post(url=f"{triple_store['url']}:{triple_store['port']}/repositories/{triple_store['repository']}/statements",
                                                     data = query, params=params, auth=(triple_store['username'], triple_store['password']), headers={'Content-Type': 'application/sparql-update'}))
@@ -84,7 +64,7 @@ def post_query(triple_store: dict, query: str, accept: str, params: dict = None)
         'Content-Type': 'application/sparql-query', 
         'Accept': accept
     }
-    add_graph_to_params(params, triple_store["input_graph"])
+    params = add_graph_to_params(params, triple_store["input_graph"])
     try:
         return manage_graphdb_response(requests.post(url=f"{triple_store['url']}:{triple_store['port']}/repositories/{triple_store['repository']}",
                                                     data = query, params=params, auth=(triple_store['username'], triple_store['password']), headers=headers))
@@ -96,4 +76,5 @@ def add_graph_to_params(params, graph):
         params['default-graph-uri'] = graph
     else:
         params = {'default-graph-uri': graph}
+    return params
 
