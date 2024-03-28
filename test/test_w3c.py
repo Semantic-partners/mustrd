c4d909c0c350b710bc367c2d7e8a236b83073c16
apicouSP <aymeric.picou@semanticpartners.com>
2024-03-28 11:45:36 +0100

    delete triplestore w3c compliance tests

--- test/test_w3c.py
+++
@@ -1,8 +0,0 @@
-from mustrd.mustrdTestPlugin import run_test_spec
-
-
-def test_w3c_gdb(w3c_tests):
-    assert run_test_spec(w3c_tests)
-    
-def test_w3c_rdflib(w3c_tests):
-    assert run_test_spec(w3c_tests)
