6e6bb44b987103e6a7c4666147cf6108fa65c1d2
apicouSP <aymeric.picou@semanticpartners.com>
2024-04-09 11:33:26 +0200

    Feature: no need to write test functions
    Issue:
    tests are closely linked to files. We cannot just give callable to pytest since pytest is checking that files exists.
    Workaround that needs to rewrite some pytest code

--- test/test_run_specs.py
+++ test/test_run_specs.py
@@ -1,9 +1,5 @@
 from mustrd.mustrdTestPlugin import run_test_spec
 
 
-def test_unit_rdflib(unit_tests):
-    assert run_test_spec(unit_tests)
-
-
-def test_unit_gdb(unit_tests):
+def test_unit(unit_tests):
     assert run_test_spec(unit_tests)
