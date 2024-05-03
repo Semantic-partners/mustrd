9b14c8e530ebb7071744614d50298aae54cdde6c
apicouSP <aymeric.picou@semanticpartners.com>
2024-05-03 09:07:32 +0200

    Revert "feat: mustrd query processor: allow changing query directly with objects"
    This reverts commit 110687f03120991206e4b528a95c6c1baa6ed32e.

--- mustrd/mustrdQueryProcessor.py
+++ mustrd/mustrdQueryProcessor.py
@@ -15,76 +15,59 @@ from builtins import list, set, tuple, str
 namespace = "https://mustrd.com/query/"
 
 class MustrdQueryProcessor:
-    original_query : Union [Query, ParseResults]
-    current_query : Union [Query, ParseResults] 
-    graph = Graph()
-    algebra_mode: bool = False
-    graph_mode: bool = True
+    original_query : Union [Query, ParseResults] 
+    original_query : Query
+    query_graph = Graph()
+    is_algebra_graph: bool = False
 
-    def __init__(self, query_str: str, algebra_mode: bool = False, graph_mode: bool = True):
+    def __init__(self, query_str: str, is_algebra_graph: bool = False):
         parsetree = parseQuery(query_str)
-        # Init original query to algebra or parsed query
-        self.original_query = (algebra_mode and translateQuery(parsetree)) or parsetree
-        self.current_query = self.original_query
-        self.algebra_mode = algebra_mode
-        self.graph_mode = graph_mode
-        if graph_mode:
-            self.query_to_graph((algebra_mode and self.original_query.algebra) or parsetree._toklist, BNode())
+        self.original_query = (is_algebra_graph and translateQuery(parsetree)) or parsetree
+        self.is_algebra_graph = is_algebra_graph
+        
+        self.query_to_graph((is_algebra_graph and self.original_query.algebra) or parsetree._toklist, BNode())
 
     def query_to_graph(self, part: CompValue, partBnode):
         if not part or not partBnode:
             return
-        self.graph.add((partBnode, RDF.type, URIRef(namespace + type(part).__name__)))
-        self.graph.add((partBnode, QUERY.has_class , Literal(str(part.__class__.__name__))))
+        self.query_graph.add((partBnode, RDF.type, URIRef(namespace + type(part).__name__)))
+        self.query_graph.add((partBnode, QUERY.has_class , Literal(str(part.__class__.__name__))))
         if isinstance(part, CompValue) or isinstance(part, ParseResults):
-            self.graph.add((partBnode, QUERY.name , Literal(part.name)))
+            self.query_graph.add((partBnode, QUERY.name , Literal(part.name)))
         if isinstance(part, CompValue):
             for key, sub_part in part.items():
                 sub_part_bnode = BNode()
-                self.graph.add((partBnode, URIRef(namespace + str(key)) , sub_part_bnode))
+                self.query_graph.add((partBnode, URIRef(namespace + str(key)) , sub_part_bnode))
                 self.query_to_graph(sub_part, sub_part_bnode)
         elif hasattr(part, '__iter__') and not isinstance(part, Identifier) and not isinstance(part, str):
             for sub_part in part:
                 sub_part_bnode = BNode()
-                self.graph.add((partBnode, QUERY.has_list , sub_part_bnode))
+                self.query_graph.add((partBnode, QUERY.has_list , sub_part_bnode))
                 self.query_to_graph(sub_part, sub_part_bnode)
         elif isinstance(part, Identifier) or isinstance(part, str):
-                self.graph.add((partBnode, QUERY.has_value, Literal(part)))
+                self.query_graph.add((partBnode, QUERY.has_value, Literal(part)))
 
-    def serialize_graph(self):
-        if not self.graph_mode:
-            raise Exception("Not able to execute that function if graph mode is not activated: cannot work with two sources of truth")
-        return self.graph.serialize(format = "ttl")
+    def serialize(self):
+        return self.query_graph.serialize(format = "ttl")
     
-    def query_graph(self, meta_query: str):
-        if not self.graph_mode:
-            raise Exception("Not able to execute that function if graph mode is not activated: cannot work with two sources of truth")
-        return self.graph.query(meta_query)
+    def query(self, meta_query: str):
+        return self.query_graph.query(meta_query)
     
     def update(self, meta_query: str):
-        if not self.graph_mode:
-            # Implement update directly on objects: self.current_query
-            pass
-        return self.graph.update(meta_query)
+        return self.query_graph.update(meta_query)
     
     def get_query(self):
-        if self.graph_mode:
-            roots = self.graph.query("SELECT DISTINCT ?sub WHERE {?sub ?prop ?obj FILTER NOT EXISTS {?s ?p ?sub}}")
-            if len(roots) != 1:
-                raise Exception("query graph has more than one root: invalid")
-            
-            for root in roots:
-                new_query = self.graph_to_query(root.sub)
-            if not self.algebra_mode:
-                new_query = ParseResults(toklist=new_query, name=self.original_query.name)
-                new_query = translateQuery(new_query)
-            else:
-                new_query = Query(algebra=new_query, prologue=self.original_query.prologue)
+        roots = self.query_graph.query("SELECT DISTINCT ?sub WHERE {?sub ?prop ?obj FILTER NOT EXISTS {?s ?p ?sub}}")
+        if len(roots) != 1:
+            raise Exception("query graph has more than one root: invalid")
+        
+        for root in roots:
+            new_query = self.graph_to_query(root.sub)
+        if not self.is_algebra_graph:
+            new_query = ParseResults(toklist=new_query, name=self.original_query.name)
+            new_query = translateQuery(new_query)
         else:
-            if not self.algebra_mode:
-                new_query = translateQuery(self.current_query)
-            else:
-                new_query = self.current_query
+            new_query = Query(algebra=new_query, prologue=self.original_query.prologue)
         return translateAlgebra(new_query)
         
         
@@ -114,7 +97,7 @@ class MustrdQueryProcessor:
 
     def get_subject_dict(self, subject):
         dict = {}
-        for key, value in self.graph.predicate_objects(subject):
+        for key, value in self.query_graph.predicate_objects(subject):
             # If key already exists: create or add to a list
             if key == QUERY.has_list:
                 if key in dict:
