596660008c9405cdb4b3e0a4545f92f514a40d4e
apicouSP <aymeric.picou@semanticpartners.com>
2024-08-22 17:37:56 +0200

    fix flake8 issues

--- test/addspec_source_file_to_spec_graph.py
+++ test/addspec_source_file_to_spec_graph.py
@@ -3,11 +3,11 @@ from rdflib import Graph, Literal, URIRef
 
 
 def parse_spec(spec: str, spec_uri: URIRef, filename: str):
-        spec_graph = Graph().parse(data=spec, format='ttl')
+        spec_graph = Graph().parse(data=spec, format='ttl') # noqa E117
 
         addspec_source_file_to_spec_graph(spec_graph, spec_uri, filename)
         return spec_graph
 
 
 def addspec_source_file_to_spec_graph(spec_graph: Graph, spec_uri: URIRef, filename: str):
-        spec_graph.add([spec_uri, MUST.specSourceFile, Literal(filename)])
+        spec_graph.add([spec_uri, MUST.specSourceFile, Literal(filename)]) # noqa E117
